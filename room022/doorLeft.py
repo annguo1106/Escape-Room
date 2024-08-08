9094d2be1c22c0550972890609740a77d67033ed
annguo1106 <annguo1106@gmail.com>
2024-08-08 17:20:12 +0800

    tmp

--- room022/doorLeft.py
+++ room022/doorLeft.py
@@ -7,6 +7,7 @@ class DoorLeft(sceneUtil.Scenes):
         super().__init__()
         self.direction = ["Back", "MainTheme", "None", "None"]
         self.scene_name = "doorLeft"
+        self.box_code = []
     
     def setBackground(self):
         current_path = os.path.dirname(os.path.abspath(__file__))
@@ -15,6 +16,18 @@ class DoorLeft(sceneUtil.Scenes):
         background.center_x = 561
         background.center_y = 325
         self.scene.add_sprite("Background", background)
+    
+    def setup(self):
+        super().setup()
+        
+        # box decoding
+        
+    def on_draw(self):
+        super().on_draw()
+        
+        # box decoding
+        if item_list["doorLeft"][0]["state"] == 1:
+            self.scene["box_code"].draw()
         
     def on_mouse_press(self, x: int, y: int, button: int, modifires: int):
         super().on_mouse_press(x, y, button, modifires)
@@ -25,15 +38,19 @@ class DoorLeft(sceneUtil.Scenes):
         
         # click on box
         if(box.collides_with_point((x, y))):
-            path = os.path.join(current_path, '..', item_list["doorLeft"][0]["pathShow"])
-            box.texture = arcade.load_texture(path)
-            box.scale = 0.5
-            box.position = (550, 325)
-            self.pre_action = "click box"
-            item_list["doorLeft"][0]["state"] = 1
+            # decoding
+            if item_list["doorLeft"][0]["state"] == 1:
+                pass
+            # before decoding
+            elif item_list["doorLeft"][0]["state"] == 0:    
+                path = os.path.join(current_path, '..', item_list["doorLeft"][0]["pathShow"])
+                box.texture = arcade.load_texture(path)
+                box.scale = 0.5
+                box.position = (550, 325)
+                self.pre_action = "click box"
+                item_list["doorLeft"][0]["state"] = 1
             
-            
-        # exist professor Yen
+        # exist box
         elif(self.pre_action == "click box" and not box.collides_with_point((x, y))):
             path = os.path.join(current_path, '..', item_list["doorLeft"][0]["pathSmall"])
             box.texture = arcade.load_texture(path)
